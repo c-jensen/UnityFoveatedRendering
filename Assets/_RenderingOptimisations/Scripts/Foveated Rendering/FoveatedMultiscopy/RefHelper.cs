1a0a25f25427e1cce9c18b8570fc43ba32531c4f
c-jensen <mail.christopher.jeensen@gmail.com>
2017-07-20 15:58:40 +0200

    TAA now working and resolution now must be 1920x1080

--- Assets/_RenderingOptimisations/Scripts/Foveated Rendering/FoveatedMultiscopy/RefHelper.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class RefHelper : MonoBehaviour      //this script is used to hold references to two render textures for the foveated rendering
-{
-	public CreateRenderTexture screenRendTex;
-	public CreateRenderTexture ldRendTex;
-}
