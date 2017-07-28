85d1be438d8894f96223dfcc39970cb6b8744b28
unknown <chgeske@techfak.uni-bielefeld.de>
2017-07-28 18:43:40 +0200

    Rendertexturesize corrected. Some strange camerabug occurs after starting the application

---
+++ Assets/_RenderingOptimisations/Scripts/Foveated Rendering/SetResolution.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution(2160, 1200, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
