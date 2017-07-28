44cd7e032b405872a47843bce661b6a81bec56d4
unknown <chgeske@techfak.uni-bielefeld.de>
2017-07-28 18:43:14 +0200

    Rendertexturesize corrected. Some strange camerabug occurs after starting the application

--- Assets/_RenderingOptimisations/Scripts/Foveated Rendering/RenderTexturesDrawingOrder.cs
+++ Assets/_RenderingOptimisations/Scripts/Foveated Rendering/RenderTexturesDrawingOrder.cs
@@ -20,9 +20,7 @@ public class RenderTexturesDrawingOrder : MonoBehaviour
 		GL.PushMatrix();
         if (Event.current.type.Equals(EventType.Repaint))//only draw once a frame
         {
-			Graphics.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), outerTexture);     //if the render texture is from the whole screen draw the render texture full screen
-
-            //if not, draw it appropriately for foveated rendering at the right size and location.
+			Graphics.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), outerTexture);     
             Graphics.DrawTexture(new Rect(innerViewport.focusPoint.x - (middleTexture.width / 2), Screen.height - innerViewport.focusPoint.y - (middleTexture.height / 2), middleTexture.width, middleTexture.height), middleTexture);
             Graphics.DrawTexture(new Rect(innerViewport.focusPoint.x - (innerTexture.width/2), Screen.height - innerViewport.focusPoint.y - (innerTexture.height/2), innerTexture.width, innerTexture.height), innerTexture);
 		}
